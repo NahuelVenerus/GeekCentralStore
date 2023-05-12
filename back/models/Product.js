5ed084f438309ffdd4b772972365caedb86b98ee
RocioRomano <rociobromano95@gmail.com>
2023-05-12 13:35:57 -0300

    ModelProduct-v1

---
+++ back/models/Product.js
@@ -0,0 +1,23 @@
+const Sequelize = require("sequelize");
+const db = require("../db");
+
+class Product extends Sequelize.Model {}
+
+Product.init(
+  {
+    nombre: { type: Sequelize.STRING, allowNull: false },
+    precio: { type: Sequelize.FLOAT, allowNull: false },
+    descripcion: { type: Sequelize.STRING, defaultValue: "" },
+    valoracion: { type: Sequelize.FLOAT, defaultValue: 0 },
+    imagen: {
+      type: Sequelize.STRING,
+      defaultValue:
+        "https://www.webstoresl.com/sellercenter/assets/images/no-product-image.png",
+    },
+    cantVentas: { type: Sequelize.INTEGER, defaultValue: 0 },
+    categoria: { type: Sequelize.STRING, defaultValue: [] },
+  },
+  { sequelize: db, modelName: "product" }
+);
+
+module.exports = Product;
