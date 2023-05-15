32584a0c968e9cea898454dad4766b7c4c99207d
NahuelVenerus <NahuelVenerus>
2023-05-15 19:41:05 -0300

    modelos y rutas de producto carrito

--- back/server.js
+++ back/server.js
@@ -13,6 +13,6 @@ app.use("/api", router);
 
 const PORT = 3001;
 
-db.sync({ force: false }).then(() =>
+db.sync({ force: true }).then(() =>
   app.listen(PORT, () => console.log("Listening on port: ", PORT))
 );
