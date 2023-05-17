c7246acdac09622d4eab6cbd1c0e305b389b34be
NahuelVenerus <NahuelVenerus>
2023-05-17 11:32:46 -0300

    cambios en la navbar si el usuario esta logueado

--- front/src/components/Navbar.jsx
+++ front/src/components/Navbar.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Buscador from "./Buscador";
 import { setUser } from "../state/user";
 import axios from "axios";
 import { BASE_ROUTE } from "../rutas";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
 function Navbar() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const user = useSelector((state) => state.user);
 
   const handleLogOut = (e) => {
     e.preventDefault();
@@ -25,23 +25,29 @@ function Navbar() {
   return (
     <nav>
       <div>
-        <Link to="/login">
-          <button>Login</button>
-        </Link>
-
-        <Link to="/logout">
-          <button onClick={handleLogOut}>Logout</button>
-        </Link>
-
-        <Link to="/signup">
-          <button>Signup</button>
-        </Link>
-
-        <Link to="/carrito">
-          <button>Carrito</button>
-        </Link>
+        {!user ? (
+          <div>
+            <Link to="/">
+              <button>Home</button>
+            </Link>
+            <Link to="/login">
+              <button>Login</button>
+            </Link>
+            <Link to="/signup">
+              <button>Signup</button>
+            </Link>
+          </div>
+        ) : (
+          <div>
+            <Link to="/logout">
+              <button onClick={handleLogOut}>Logout</button>
+            </Link>
+            <Link to="/carrito">
+              <button>Carrito</button>
+            </Link>
+          </div>
+        )}
       </div>
-      <Buscador />
     </nav>
   );
 }
