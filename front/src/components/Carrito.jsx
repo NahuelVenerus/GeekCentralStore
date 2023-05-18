567a8de3066f6f9d785e0ccfb19ba26c3343b7e9
berrubino <berni.rubino@gmail.com>
2023-05-18 11:40:55 -0300

    inicio vistas carrito

--- front/src/components/Carrito.jsx
+++ front/src/components/Carrito.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import axios from "axios";
+import { Carousel } from "react-bootstrap";
+import { fakeData } from "../utils/fakeData";
+import CarouselCard from "./CarouselCard";
 
 function Carrito() {
-  return <div>Carrito</div>;
+  const productos = [...fakeData];
+
+  return (
+    <Carousel>
+      {productos.map((p) => {
+        <CarouselCard producto={p} />;
+      })}
+    </Carousel>
+  );
 }
 
 export default Carrito;
