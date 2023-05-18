7e3a493c589b1e739003475ecea201d119bd62a0
NahuelVenerus <NahuelVenerus>
2023-05-18 12:03:11 -0300

    agregados botones al carrito

--- front/src/components/Carrito.jsx
+++ front/src/components/Carrito.jsx
@@ -1,19 +1,35 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Navigation } from "swiper";
 import "swiper/swiper-bundle.min.css";
-import { fakeData } from "../utils/fakeData";
 import CarritoCard from "../commons/CarritoCard";
+import { fakeData } from "../utils/fakeData";
+
+SwiperCore.use([Navigation]);
 
 export default function Carrito() {
   const products = fakeData;
+  const navigationPrevRef = useRef(null);
+  const navigationNextRef = useRef(null);
 
   return (
-    <Swiper slidesPerView={3} spaceBetween={20}>
-      {products.map((product) => (
-        <SwiperSlide key={product.id}>
-          <CarritoCard product={product} />
-        </SwiperSlide>
-      ))}
-    </Swiper>
+    <div className="swiper-container">
+      <Swiper
+        slidesPerView={3}
+        spaceBetween={20}
+        navigation={{
+          prevEl: navigationPrevRef.current,
+          nextEl: navigationNextRef.current,
+        }}
+      >
+        {products.map((product) => (
+          <SwiperSlide key={product.id}>
+            <CarritoCard product={product} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      <div ref={navigationPrevRef} className="swiper-button-prev" />
+      <div ref={navigationNextRef} className="swiper-button-next" />
+    </div>
   );
 }
