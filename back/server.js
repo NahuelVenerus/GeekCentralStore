3aef41e1d908d2084a7507e6078feaaed75fd870
Ivan Correa <ivan.correa24@yahoo.com>
2023-05-20 12:14:48 -0300

    environment-variables-added

--- back/server.js
+++ back/server.js
@@ -1,16 +1,15 @@
+require("dotenv").config();
 const express = require("express");
+const PORT = process.env.SERVER_PORT;
 const db = require("./config/db/index");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const router = require("./routes");
 const app = express();
-
 app.use(cookieParser());
 app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(express.json());
 app.use("/api", router);
-
-const PORT = 3001;
 
 db.sync({ force: false }).then(() =>
   app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
