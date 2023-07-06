9ccdc1bc80a686a1958296a5e592a5a90c3ffbff
Nahuel Venerus <nahuelvenerus@gmail.com>
2023-07-06 18:42:26 -0300

    front origin fixed deploy

--- back/server.js
+++ back/server.js
@@ -7,8 +7,11 @@ const router = require("./routes");
 const app = express();
 const PORT = process.env.SERVER_PORT;
 
+const ORIGIN =
+  "https://geek-central-store-front.onrender.com" || "http://localhost:3000";
+
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: ORIGIN, credentials: true }));
 app.use(express.json());
 app.use("/api", router);
 
